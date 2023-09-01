4a12838afefe016d978fd6655ea628d10280fac0
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-08-31 21:29:46 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 31 Aug 2023 23:53:21 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#525252",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8b5cf6"
+};
